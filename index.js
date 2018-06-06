19a4fb63cb8d34c63aa7db31ea65007f3711913c
edmobe <e.d.mobe@hotmail.com>
2018-06-05 22:59:40 -0600

    add functional application

---
+++ index.js
@@ -0,0 +1,52 @@
+// Importing modules
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var cors = require('cors');
+var path = require('path');
+
+var app = express();
+
+const route = require('./routes/route');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/contactlist');
+
+// On connection
+mongoose.connection.on('connected', () =>{
+    console.log('Connected to database mongodb @ 27017');
+});
+
+// On connection
+mongoose.connection.on('error', (err) =>{
+    if(err)
+    {
+        console.log('Error in Database connection: ' + err);
+    }
+    console.log('Connected to database mongodb @ 27017');
+});
+
+// Port number
+const port = 3000;
+
+// Adding middleware - cors
+app.use(cors());
+
+// Body - parser
+app.use(bodyparser.json());
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api', route);
+
+// Testing server
+app.get('/', (req, res)=>{
+    res.send('foobar');
+});
+
+// Binding server with the port
+app.listen(port, ()=> {
+    console.log('Server started at port: ' + port);
+});
